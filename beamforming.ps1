1bbe9916701080284a6dd2649746dff0dfd42b54
Justin Rohweller <jrohweller13@apu.edu>
2017-06-28 14:32:12 -0700

    Powershell Commands
    This runs C#, converts .raw output to .wav file using SoX converter (find on web), and then runs .jar file of our java project.

---
+++ beamforming.ps1
@@ -0,0 +1,27 @@
+﻿function beam()
+{
+write-host("Hello there how are you")
+
+cd \
+cd Users\Justin\Documents\
+cd '.\Visual Studio 2015\'
+cd Projects\ConsoleApplication1\ConsoleApplication1\bin\Debug
+sleep 1
+Invoke-Item ConsoleApplication1.exe
+
+
+sleep 2
+sox -r 2048000 -e unsigned -b 8 -c 2 filename1.raw outputfile.wav
+
+sleep 1
+cd \Users\Justin\Desktop
+java -jar BeamForm2.jar
+
+
+
+sleep 1
+cd \Users\Justin
+
+}
+
+beam
